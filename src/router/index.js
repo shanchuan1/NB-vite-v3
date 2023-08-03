8fbc86f123ec316d4a84810e4d7f48feec7a8b54
zhongqifeng <zhongqifeng@yunquna.com>
2023-08-03 21:50:15 +0800

    fix:update

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory ,createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/Home.vue'
 
 const routes = [
   {
@@ -14,48 +14,48 @@ const routes = [
   {
     path: '/login',
     name: 'login',
-    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
+    component: () => import(/* webpackChunkName: "login" */ '@/views/Login.vue')
   },
   {
     path: '/category',
     name: 'category',
-    component: () => import(/* webpackChunkName: "category" */ '../views/Category.vue')
+    component: () => import(/* webpackChunkName: "category" */ '@/views/Category.vue')
   },
   {
     path: '/cart',
     name: 'cart',
-    component: () => import(/* webpackChunkName: "cart" */ '../views/Cart.vue')
+    component: () => import(/* webpackChunkName: "cart" */ '@/views/Cart.vue')
   },
   {
     path: '/user',
     name: 'user',
-    component: () => import(/* webpackChunkName: "user" */ '../views/User.vue')
+    component: () => import(/* webpackChunkName: "user" */ '@/views/User.vue')
   },
   {
     // path: '/product/:id',
     path: '/product',
     name: 'product',
-    component: () => import(/* webpackChunkName: "product" */ '../views/ProductDetail.vue')
+    component: () => import(/* webpackChunkName: "product" */ '@/views/ProductDetail.vue')
   },
   {
     path: '/create-order',
     name: 'create-order',
-    component: () => import(/* webpackChunkName: "create-order" */ '../views/CreateOrder.vue')
+    component: () => import(/* webpackChunkName: "create-order" */ '@/views/CreateOrder.vue')
   },
   {
     path: '/address',
     name: 'address',
-    component: () => import(/* webpackChunkName: "address" */ '../views/Address.vue')
+    component: () => import(/* webpackChunkName: "address" */ '@/views/Address.vue')
   },
   {
     path: '/address-edit',
     name: 'address-edit',
-    component: () => import(/* webpackChunkName: "address-edit" */ '../views/AddressEdit.vue')
+    component: () => import(/* webpackChunkName: "address-edit" */ '@/views/AddressEdit.vue')
   },
   {
     path: '/search',
     name: 'search',
-    component: () => import(/* webpackChunkName: "search" */ '../views/Search.vue')
+    component: () => import(/* webpackChunkName: "search" */ '@/views/Search.vue')
   },
 ]
 
