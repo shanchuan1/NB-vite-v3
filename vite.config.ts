c94e5531d5610b2baa62c4a1c4c5df325341cf8c
zhongqifeng <zhongqifeng@yunquna.com>
2023-08-04 12:31:08 +0800

    fix:update

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,17 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 8081,
+    https: false,
+    proxy: {
+      '/api': {
+        target: 'http://172.17.28.177',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src') // 设置别名
