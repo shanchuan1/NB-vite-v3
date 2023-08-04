f108d726703b2d04e89861e51a54829fdc0f4c0e
zhongqifeng <zhongqifeng@yunquna.com>
2023-08-04 22:53:53 +0800

    fix:update

--- vite.config.ts
+++ vite.config.ts
@@ -7,17 +7,17 @@ export default defineConfig({
   
   base:'./',// 将根路径换成相对路径
   plugins: [vue()],
-  server: {
-    port: 8080,
-    https: false,
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  },
+  // server: {
+  //   port: 8080,
+  //   https: false,
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://127.0.0.1',
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, '')
+  //     }
+  //   }
+  // },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src') // 设置别名
