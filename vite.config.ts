03174fa1b74ea9e15e2d08cb66799ae88f7dfc5e
zhongqifeng <zhongqifeng@yunquna.com>
2023-08-05 12:34:01 +0800

    fix:update

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,15 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  
   base:'./',// 将根路径换成相对路径
   plugins: [vue()],
   server: {
-    port: 8080,
+    // port: 8080,
     https: false,
     proxy: {
       '/api': {
-        target: 'http://backend-api-01.newbee.ltd',
+        // target: 'http://backend-api-01.newbee.ltd',
+        target: '//backend-api-01.newbee.ltd/api/v1',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
